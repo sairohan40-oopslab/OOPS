17e2f57fbd18db1861c5d6d29b7fd07cc6bc0f34
M.Sai Rohan <rohansai7654321@gmail.com>
2025-02-16 13:49:13 +0530

    Add files via upload

---
+++ ex1/multiplication in reverse order.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseMultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        System.out.print("Enter the range: ");
+        int range = scanner.nextInt();
+        System.out.println("Multiplication Table of " + num + " in Reverse Order:");
+        for (int i = range; i >= 1; i--) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+
+        scanner.close();
+    }
+}
