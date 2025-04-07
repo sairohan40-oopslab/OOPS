98c282daee80f95937b763f45f4a131f00950958
M.Sai Rohan <rohansai7654321@gmail.com>
2025-04-07 09:48:28 +0530

    Update Reverse multlipication.java

--- simple java programs/Reverse multlipication.java
+++ simple java programs/Reverse multlipication.java
@@ -1,22 +1,14 @@
 import java.util.Scanner;
-public class BMICalculator {
-public static void main(String[] args)
-{ Scanner scanner = new
-Scanner(System.in);
-System.out.print("Enter weight in kilograms: ");
-double weight = scanner.nextDouble();
-System.out.print("Enter height in meters: ");
-double height = scanner.nextDouble();
-double bmi = weight / (height * height);
-System.out.printf("Your BMI is: %.2f\n", bmi);
-if (bmi < 18.5) {
-System.out.println("Category: Underweight");
-} else if (bmi < 24.9)
-{ System.out.println("Category: Normal weight");
-} else if (bmi < 29.9)
-{ System.out.println("Category: Overweight");
-} else {
-System.out.println("Category: Obese");
+public class ReverseMultiplicationTable
+{ public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int num = scanner.nextInt();
+System.out.print("Enter the range: ");
+int range = scanner.nextInt();
+System.out.println("Multiplication Table of " + num + " in Reverse Order:");
+for (int i = range; i >= 1; i--) {
+System.out.println(num + " x " + i + " = " + (num * i));
 }
 scanner.close();
 }
